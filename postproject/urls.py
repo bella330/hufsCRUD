ce8e39b1a8ca5feab36bfcd21e75af2aa5bffa3d
namu0423 <bbj0423@likelion.org>
2019-04-09 02:24:50 +0900

    first commit

--- postproject/urls.py
+++ postproject/urls.py
@@ -5,12 +5,12 @@ import blogapp.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.home, name="home"),
-    path('blog/빈칸', blogapp.views.detail, name='detail'),
+    path('blog/<int:빈칸>', blogapp.views.detail, name='detail'),
     path('blog/new/', blogapp.views.new, name='new'),
     path('blog/create/', blogapp.views.create, name='create'),
-    path('blog/delete/빈칸/', blogapp.views.delete, name='delete'),
-    path('blog/update/빈칸/', blogapp.views.update, name='update'),
-    path('blog/edit/빈칸/', blogapp.views.edit, name='edit'),
+    path('blog/delete/<int:빈칸>/', blogapp.views.delete, name='delete'),
+    path('blog/update/<int:빈칸>/', blogapp.views.update, name='update'),
+    path('blog/edit/<int:빈칸>/', blogapp.views.edit, name='edit'),
 
     
 ]
