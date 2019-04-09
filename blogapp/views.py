5a6602581eca5bcc89a974f10e1a0d12c14c8ee4
namu0423 <bbj0423@likelion.org>
2019-04-09 12:12:02 +0900

    ㅊ꼉 빈칸채우기

--- blogapp/views.py
+++ blogapp/views.py
@@ -6,8 +6,8 @@ def home(request):
     빈칸 = Blog.objects
     return render(request, 'home.html', {빈칸: 빈칸})
 
-def detail(request, blog_id):
-    빈칸 = get_object_or_404(Blog, pk=blog_id)
+def detail(request, 빈칸):
+    빈칸 = get_object_or_404(Blog, pk=빈칸)
     return render(request, 'detail.html', {빈칸: 빈칸})
 
 def new(request):
@@ -21,17 +21,17 @@ def create(request):
     blog.save()
     return redirect('/blog/'+str(blog.id))
 
-def delete(request, blog_id):
-    destroy = get_object_or_404(Blog, pk=blog_id)
+def delete(request, 빈칸):
+    destroy = get_object_or_404(Blog, pk=빈칸)
     destroy.delete()
     return redirect('home')
 
-def update(request, blog_id):
-    빈칸 = get_object_or_404(Blog, pk=blog_id)
+def update(request, 빈칸):
+    빈칸 = get_object_or_404(Blog, pk=빈칸)
     return render(request, 'update.html', {빈칸: 빈칸})
 
-def edit(request, blog_id):
-    edit = Blog.objects.get(pk=blog_id)
+def edit(request, 빈칸):
+    edit = Blog.objects.get(pk=빈칸)
     edit.title = request.POST[빈칸]
     edit.body = request.POST[빈칸]
     edit.pub_date = timezone.datetime.now()
