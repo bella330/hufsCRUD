14f11eaf3ee52008cd33605c3b97f79089d0ea59
namu0423 <bbj0423@likelion.org>
2019-04-09 13:06:42 +0900

    수정

--- blogapp/views.py
+++ blogapp/views.py
@@ -4,11 +4,11 @@ from .models import Blog
 
 def home(request):
     빈칸 = Blog.objects
-    return render(request, 'home.html', {빈칸: 빈칸})
+    return render(request, 'home.html', {"posts": 빈칸})
 
-def detail(request, 빈칸):
+def detail(request, a):
     빈칸 = get_object_or_404(Blog, pk=빈칸)
-    return render(request, 'detail.html', {빈칸: 빈칸})
+    return render(request, 'detail.html', {빈칸: post})
 
 def new(request):
     return render(request, 'new.html')
@@ -21,16 +21,16 @@ def create(request):
     blog.save()
     return redirect('/blog/'+str(blog.id))
 
-def delete(request, 빈칸):
+def delete(request, b):
     destroy = get_object_or_404(Blog, pk=빈칸)
     destroy.delete()
     return redirect('home')
 
-def update(request, 빈칸):
+def update(request, c):
     빈칸 = get_object_or_404(Blog, pk=빈칸)
-    return render(request, 'update.html', {빈칸: 빈칸})
+    return render(request, 'update.html', {빈칸: text})
 
-def edit(request, 빈칸):
+def edit(request, d):
     edit = Blog.objects.get(pk=빈칸)
     edit.title = request.POST[빈칸]
     edit.body = request.POST[빈칸]
